e8ebafb1639267da72dac0845851805fb1f4c442
Felipy Camargo de Souza <felipy.souza@itajai.quay.com.br>
2020-05-15 17:31:10 -0300

    autenticação usuário, falta corrigir a validação da rota de atualizar o usuário se os campos da response não estão vazios ou se são obrigatórios

--- gobarber/back-end/src/app/middlewares/auth.js
+++ gobarber/back-end/src/app/middlewares/auth.js
@@ -1,8 +1,11 @@
 import jwt from 'jsonwebtoken';
+import {promisify} from 'util';
+
+import UserController from '../controllers/UserController';
 
 import authConfig from '../../config/auth';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if(!authHeader){
@@ -12,12 +15,13 @@ export default (req, res, next) => {
     const [, token] = authHeader.split(' '); 
     
     try {
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+        
+        req.userId = decoded.id;
 
+        return next();
     }catch(e){
         return res.status(401).json({error: 'Token invalid'});
     }
-    
-    console.log(authHeader);
 
-    return next();
 };
